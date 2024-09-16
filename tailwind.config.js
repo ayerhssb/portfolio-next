9fe2b4c671a21cbaabdca53a267d7297803054ee
ayerhssb <shreyabhartissb1@gmail.com>
2024-09-16 22:53:34 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+const {fontFamily} = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)'],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "f5f5f5",
+        primary: "#863E96",
+        primaryDark: "#58E6D9",
+      }
+    },
+  },
+  plugins: [],
+}
+
